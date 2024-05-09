d475d8c80f484eacc699dccce822e4c8275215b0
sung-ju-park <tjdwn9386@naver.com>
2024-05-09 16:07:29 +0900

    회원가입, 로그인 라우터 추가

---
+++ data/auth.js
@@ -0,0 +1,28 @@
+let users = [
+    {
+        id: '1',
+        username: 'apple',
+        password: '1111',
+        name: '김사과',
+        email: 'apple@apple.com',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
+    }
+];
+
+export async function createUser(username, password, name, email){
+    const user = {
+        id: '10',
+        username,
+        password,
+        name,
+        email,
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
+    }
+    users = [user, ...users]
+    return users;
+}
+
+export async function login(username){
+    const user = users.find((user) => user.username === username)
+    return user;
+}
