67a70ea2a5030bc5c333142e27001b3a12ef5e0c
sung-ju-park <tjdwn9386@naver.com>
2024-05-09 16:09:19 +0900

    토큰 검증

--- data/auth.js
+++ data/auth.js
@@ -6,20 +6,31 @@ let users = [
         name: '김사과',
         email: 'apple@apple.com',
         url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
+    },
+    {
+        id: '2',
+        username: 'banana',
+        password: '$2b$10$ERrMBARDprS1Ajt9snjS2uXaCuYv/enG3nWFldAQWsnUQ2G.XwKvu',
+        name: '반하나',
+        email: 'banana@banana.com',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s' 
     }
 ];
 
-export async function createUser(username, password, name, email){
-    const user = {
-        id: '10',
-        username,
-        password,
-        name,
-        email,
-        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJSRyel4MCk8BAbI6gT_j4DBTEIcY0WW4WWfoklymsWA&s'
-    }
-    users = [user, ...users]
-    return users;
+// 아이디(username) 중복검사
+export async function findByUsername(username){
+    return users.find((user) => user.username === username);
+}
+
+// id 중복검사
+export async function findById(id){
+    return users.find((user) => user.id === id);
+}
+
+export async function createUser(user){
+    const created = {id:'10', ...user }
+    users.push(created);
+    return created.id;
 }
 
 export async function login(username){
