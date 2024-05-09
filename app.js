f60fd35c88e0d7469774a4ff432e0d28ca53014d
sung-ju-park <tjdwn9386@naver.com>
2024-05-09 16:13:40 +0900

    Sequelize 적용

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from "./config.js";
-import { db } from './db/database.js';
+import { sequelize } from "./db/database.js";
 
 const app = express();
 
@@ -18,6 +18,6 @@ app.use((req, res, next) => {
 });
 
 // DB 연결 테스트!
-// db.getConnection().then(connection => console.log(connection));
-
-app.listen(config.host.port);
+sequelize.sync().then(() => {
+    app.listen(config.host.port);
+});
